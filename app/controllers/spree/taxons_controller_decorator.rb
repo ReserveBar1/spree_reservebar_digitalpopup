407ffcc90da1683d723df33842de18200d4a4b38
Jason Knebel <jasonkneb@gmail.com>
2015-08-13 19:15:32 -0400

    adding initial actions & views for Remy & Macallan

---
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -0,0 +1,17 @@
+Spree::TaxonsController.class_eval do
+
+  def digital_popup
+    subdomain = request.subdomain
+    brand = subdomain.split('-')[0]
+
+    if brand == 'remy'
+      @products = Spree::Brand.find_by_title('Remy Martin').products
+      render 'remy_popup', layout: false
+    elsif brand == 'macallan'
+      render 'macallan_popup', layout: false
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
+
+end
