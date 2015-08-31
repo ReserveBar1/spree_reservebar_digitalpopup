c4b615d5faa245fca5e5b66bae1ba8a032f162be
Jason Knebel <jasonkneb@gmail.com>
2015-08-31 17:34:14 -0400

    logging in guest user from user_email in params

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -1,20 +1,22 @@
 Spree::TaxonsController.class_eval do
-
   def digital_popup
     subdomain = request.subdomain
     brand = subdomain.split('-')[0]
 
     if brand == 'remy'
       session.deep_merge!(params)
+
+      # Logging in as Guest
+      user = Spree::User.anonymous!
+      user.update_attribute(:email, session[:user_email])
+      sign_in(:user, user)
+
       @products = Spree::Brand.find_by_title('Remy Martin').products
       render 'remy_popup', layout: false
-    elsif brand == 'macallan'
-      render 'macallan_popup', layout: false
     # elsif brand == 'macallan'
-    #   render 'macallan_popup', layout: 'false'
+    #   render 'macallan_popup', layout: false
     else
       raise ActionController::RoutingError.new('Not Found')
     end
   end
-
 end
