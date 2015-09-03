8de1ed28e3046188feabddb5f47194ec0d1a2e02
Jason Knebel <jasonkneb@gmail.com>
2015-09-02 20:03:33 -0400

    adding new products page

--- app/controllers/spree/digital_popups_controller.rb
+++ app/controllers/spree/digital_popups_controller.rb
@@ -11,6 +11,20 @@ class Spree::DigitalPopupsController < Spree::BaseController
     end
   end
 
+  def products
+    if @brand == 'remy'
+      permalinks = [
+        'remy-martin-vsop-1',
+        'remy-martin-1738-accord-royal-custom-engraved-bottle',
+        'remy-martin-xo-excellence-custom-engraved'
+      ]
+      @products = Spree::Product.where(permalink: permalinks)
+      render 'remy/products'
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
+
   private
 
   def get_brand
--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,13 @@ Reservebar::Application.routes.prepend do
   match '', to: 'spree/digital_popups#index', constraints: lambda { |r| r.subdomain.present? && r.subdomain.include?('-digitalpopup') }
 
 end
+
+Spree::Core::Engine.routes.prepend do
+
+  resources :digital_popups do
+    collection do
+      get :products
+    end
+  end
+
+end
